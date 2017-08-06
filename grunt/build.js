a714fa463dceee49b674e1fdb25fa2a8bdf66548
kushwanth22 <kxk16830@ucmo.edu>
2017-08-06 18:43:06 -0400

    Remove duplicated directory

--- grunt/build.js
+++
@@ -1,8 +0,0 @@
-var request = require('supertest');
-var app = require('../app.js');
-
-describe('GET /', function() {
-  it('respond with hello', function(done) {
-    request(app).get('/').expect('hello', done);
-  });
-});
